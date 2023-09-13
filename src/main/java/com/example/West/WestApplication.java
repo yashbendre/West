36a98a7a785dad93a87561d4c513952f7626b645
Yash Bendre <yashdbendre@gmail.com>
2023-09-13 15:34:30 +0530

    first push

---
+++ src/main/java/com/example/West/WestApplication.java
@@ -0,0 +1,13 @@
+package com.example.West;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/West/WestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.West;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
